cbea9bcf7a157c78b4f78d76d32b1afdcaae16a5
Mauro <maurogch@gmail.com>
2018-10-04 04:38:20 -0300

    new querries file

---
+++ dao/BD/Querries.sql
@@ -0,0 +1,11 @@
+USE neonlab1_gotoevent;
+
+/*create table Artists(
+    id_artist int auto_increment,
+    name varchar(50) not null,
+    lastname varchar (50) not null,
+    primary key (id_artist)
+);*/
+
+/*select * from Artists;*/
+
