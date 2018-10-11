dff079227a449c2d463c8f28171a8c600d165c2d
Mauro <maurogch@gmail.com>
2018-10-11 06:21:18 -0300

    deactivate seattpyes call, this should go to proper controller

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -7,8 +7,8 @@
 
 		public function __construct()
 		{
-			if(!isset($_SESSION["seatTypes"]))
-				$_SESSION["seatTypes"] = SeatsTypesDao::getInstance()->retrieveAll();
+			//if(!isset($_SESSION["seatTypes"]))
+			//	$_SESSION["seatTypes"] = SeatsTypesDao::getInstance()->retrieveAll();
 		}
 		
 		function index()
