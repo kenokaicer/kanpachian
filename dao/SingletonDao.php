4d53ce9b23aaa11c7519cf280d128fb3e3479e60
Mauro <maurogch@gmail.com>
2018-10-11 06:21:29 -0300

    fixed and added alternatives

--- dao/SingletonDao.php
+++ dao/SingletonDao.php
@@ -1,17 +1,56 @@
 <?php namespace Dao;
 
+use Exception as Exception;
+
+/*class SingletonDao
+{
+    protected static $instance = null;
+
+    public static function getInstance()
+        {
+            $cls = get_called_class(); // late-static-bound class name
+            if (!isset(self::$instance[$cls])) {
+                self::$instance[$cls] = new static;
+            }
+            return self::$instance[$cls];
+        }
+}*/
+
 class SingletonDao
 {
-    private static $instance = array();
+    private static $instances = array();
+    protected function __construct() {}
+    protected function __clone() {}
+    public function __wakeup()
+    {
+        throw new Exception("Cannot unserialize singleton");
+    }
 
     public static function getInstance()
     {
-
-        $myClass = get_called_class();
-        if (!isset(self::$instance[$myClass])) {
-            self::$instance[$myClass] = new $myClass;
+        if (!isset(self::$instances[static::class])) {
+            self::$instances[static::class] = new static;
         }
-
-        return self::$instance[$myClass];
+        return self::$instances[static::class];
     }
 }
+
+/*abstract class SingletonDao
+{
+    private static $_instances = [];
+
+    protected function __construct()
+    {
+        //Prevent construction of this class
+    }
+
+    final public static function getInstance(){
+        
+        self::$_instances[static::class] = self::$_instances[static::class] ?? new static();
+        return self::$_instances[static::class];
+    }
+
+    final private function __clone() {}
+
+    final private function __wakeup() {}
+}*/
