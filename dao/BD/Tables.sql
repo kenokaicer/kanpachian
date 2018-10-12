4b50fa899821149a306e963474d90b3741b4819e
Mauro <maurogch@gmail.com>
2018-10-11 23:01:24 -0300

    fixed contraints for pfk and changed enabled from bit to bool

--- dao/BD/Tables.sql
+++ dao/BD/Tables.sql
@@ -3,7 +3,7 @@ USE neonlab1_gotoevent;
 /*create table Categories(
     idCategory int unsigned auto_increment,
     category varchar(50) not null unique,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkCategory primary key (idCategory)
 );*/
 
@@ -12,7 +12,7 @@ USE neonlab1_gotoevent;
     eventName varchar(50) not null unique,
     idCategory int unsigned,
     constraint pkEvent primary key (idEvent),
-    enabled bit default 1,
+    enabled bool default '1',
     constraint fkCategory foreign key (idCategory) references Categories (idCategory)
 );*/
 
@@ -22,7 +22,7 @@ USE neonlab1_gotoevent;
     location varchar(80),
     image varchar(150),
     maxCapacity smallint unsigned,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkTheater primary key (idTheater)
 );*/
 
@@ -31,6 +31,7 @@ USE neonlab1_gotoevent;
     date date,
     idTheater int unsigned,
     idEvent int unsigned,
+    enabled bool default '1',
     constraint pkEventsByDate primary key (idEventsByDate),
     constraint fkTheater foreign key (idTheater) references Theaters (idTheater),
     constraint fkEvent foreign key (idEvent) references Events (idEvent)
@@ -40,32 +41,32 @@ USE neonlab1_gotoevent;
     idArtist int unsigned auto_increment,
     name varchar(50) not null,
     lastname varchar(50) not null,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkArtist primary key (idArtist)
 );*/
 
 /*create table Artists_x_EventByDate(
-    pfkArtist int unsigned,
-    pfkEventByDate int unsigned,
-    primary key (pfkArtist, pfkEventByDate),
-    foreign key (pfkArtist) references Artists (idArtist),
-    foreign key (pfkEventByDate) references EventsByDates (idEventsByDate)
+    idArtist int unsigned,
+    idEventByDate int unsigned,
+    constraint pkArtists_x_EventByDate primary key (idArtist, idEventByDate),
+    constraint pfkArtist foreign key (idArtist) references Artists (idArtist),
+    constraint pfkEventByDate foreign key (idEventByDate) references EventsByDates (idEventsByDate)
 );*/
 
 /*create table SeatTypes(
     idSeatType int unsigned auto_increment,
     name varchar(50) not null unique,
     description varchar(300),
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkSeatType primary key (idSeatType)
 );*/
 
 /*create table SeatTypes_x_Theater(
-    pfkSeatType int unsigned,
-    pfkTheater int unsigned,
-    primary key (pfkSeatType, pfkTheater),
-    foreign key (pfkSeatType) references SeatTypes (idSeatType),
-    foreign key (pfkTheater) references Theaters (idTheater)
+    idSeatType int unsigned,
+    idTheater int unsigned,
+    constraint pkSeatTypes_x_Theater primary key (idSeatType, idTheater),
+    constraint pfkSeatType foreign key (idSeatType) references SeatTypes (idSeatType),
+    constraint pfkTheater foreign key (idTheater) references Theaters (idTheater)
 );*/
 
 /*create table CreditCards(
@@ -73,7 +74,7 @@ USE neonlab1_gotoevent;
     creditCardNumber smallint unsigned not null unique,
     expirationDate date,
     cardHolder varchar(50),
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkCreditCard primary key (idCreditCard)
 );*/
 
@@ -83,7 +84,7 @@ USE neonlab1_gotoevent;
     password varchar(255) not null,
     email varchar(50) not null unique,
     role varchar(10) not null,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkUser primary key (idUser)
 );*/
 
@@ -94,7 +95,7 @@ USE neonlab1_gotoevent;
     dni smallint unsigned not null,
     idUser int unsigned not null,
     idCreditCard int unsigned,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkClient primary key (idClient),
     constraint fkUser foreign key (idUser) references Users (idUser),
     constraint fkCreditCard foreign key (idCreditCard) references CreditCards (idCreditCard)
@@ -104,7 +105,7 @@ USE neonlab1_gotoevent;
     idPurchase int unsigned auto_increment,
     date date,
     idClient int unsigned not null,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pfPurchase primary key (idPurchase),
     constraint fkClient foreign key (idClient) references Clients (idClient)
 );*/
@@ -116,7 +117,7 @@ USE neonlab1_gotoevent;
     remnants smallint unsigned not null,
     idEventsByDate int unsigned not null,
     idSeatType int unsigned not null,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkSeatByEvent primary key (idSeatsByEvent),
     constraint fkEventsByDate foreign key (idEventsByDate) references EventsByDates (idEventsByDate),
     constraint fkSeatType foreign key (idSeatType) references SeatTypes (idSeatType)
@@ -126,7 +127,7 @@ USE neonlab1_gotoevent;
     idPurchaseLine int unsigned auto_increment,
     price double unsigned not null,
     idSeatsByEvent int unsigned not null,
-    enabled bit default 1,
+    enabled bool default '1',
     constraint pkPurchaseLine primary key (idPurchaseLine),
     constraint fkSeatsByEvent foreign key (idSeatsByEvent) references SeatsByEvents (idSeatsByEvent)
 );*/
