b926332b48b3aa08a53e43521822825dff5ca0a8
maurogch <maurogch@gmail.com>
2018-10-18 21:10:22 -0300

    event list in index

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,16 +1,25 @@
 <?php
 	namespace Controllers;
 
-	use Dao\BD\SeatsTypesDao as SeatsTypesDao;
+	use Dao\BD\EventsDao as EventsDao;
 	
 	class MainController{
+		
+		private $eventsDao;
 
 		public function __construct()
 		{
+			$this->eventsDao = EventsDao::getInstance();
 		}
 		
 		function index()
-		{
+		{	
+			try{
+				$eventList = $this->eventsDao->RetrieveEventsOnly();
+			}catch(Exception $ex){
+				echo "<script> alert('Error al intentar listar Eventos: " . str_replace("'", "", $ex->getMessage()) . "');</script>";
+			}
+			var_dump($eventList);
 			require "Views/home.php";
 		}
 	}
