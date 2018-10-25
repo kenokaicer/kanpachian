eea216192687c48b49590291f61ef8c6d6db4ec4
Mauro <maurogch@gmail.com>
2018-10-25 07:09:38 -0300

    add temp back button

--- views/Admin.php
+++ views/Admin.php
@@ -7,5 +7,8 @@
         <div><button type="submit" formaction="<?=FRONT_ROOT?>ArtistManagement/index">Gestión Artistas</button></div>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>TheaterManagement/index">Gestión Teatros</button></div>
     </section>
+    <section>
+        <div><button type="submit" formaction="<?=FRONT_ROOT?>Main/index">Volver (temp)</button></div>
+    </section>
 </form>
 </div>
