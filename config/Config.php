fe6ede8ad73a37af8c544ebad3fb7e28c844d6b9
Mauro <maurogch@gmail.com>
2018-10-25 04:59:00 -0300

    BASE to FRONT_ROOT

--- config/Config.php
+++ config/Config.php
@@ -6,21 +6,18 @@ require_once "Vendor/Autoload.php";
 // Constat for Root
 define('ROOT', str_replace('\\','/',dirname(__DIR__) . "/"));
 
-// Constat for Request
-$base=explode($_SERVER['DOCUMENT_ROOT'],ROOT);
-define("FRONT_ROOT",$base[1]);
-
 // Constats for Json
 define("JSONFOLDER", "JsonFiles/");
 
-// Constats for Base de datos
+// Constats for Data Base
 define("DB_NAME", "neonlab1_gotoevent");
 define("DB_USER", "neonlab1_termo");
 define("DB_PASS", "lluviadehamburguesas");
 define("DB_HOST", "www.neonlab.com.ar");
 
 // Constats for front
-//define("FRONT_ROOT", "/UTN/MVC Example/"); //fix this constant, call FRONT_ROOT?
+$base=explode($_SERVER['DOCUMENT_ROOT'],ROOT);
+define("FRONT_ROOT",$base[1]);
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
