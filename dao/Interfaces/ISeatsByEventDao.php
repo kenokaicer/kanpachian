1faf79c86f0fe7fa034ac9827c41df31488a2aa4
Mauro <maurogch@gmail.com>
2018-10-25 11:36:10 -0300

    init

---
+++ dao/Interfaces/ISeatsByEventDao.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dao\Interfaces;
+
+use Models\SeatsByEvent as SeatsByEvent;
+
+interface ISeatsByEventDao
+{
+    public function Add(SeatsByEvent $seatsByEvent);
+    public function getByID($id);
+    public function getAll();
+    public function Update(SeatsByEvent $oldSeatsByEvent, SeatsByEvent $newSeatsByEvent);
+    public function Delete(SeatsByEvent $seatsByEvent);
+}
