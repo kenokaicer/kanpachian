5772e8249f9e72db92887bd3f202eb28ab7e2ee6
maurogch <maurogch@gmail.com>
2018-10-25 19:50:23 -0300

    comment seattype test

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,20 +1,20 @@
 <?php
 	namespace Controllers;
-	use Dao\BD\SeatsByEventDao as SeatsByEventDao;
+	/*use Dao\BD\SeatsByEventDao as SeatsByEventDao;
 	use Models\SeatsByEvent as SeatsByEvent;
-	use Models\SeatType as SeatType;
+	use Models\SeatType as SeatType;*/
 	
 	class AdminController{
 		
 		function index()
 		{	
-			$seatType = new seatType();
+			/*$seatType = new seatType();
 			$seatType->setIdSeatType(1)->setName('seat')->setDescription('desc');
 
 			$seatsByEvent = new SeatsByEvent();
 			$seatsByEvent->setQuantity(1)->setPrice(10)->setRemnants(100)->setSeatType($seatType);
 
-			SeatsByEventDao::getInstance()->add($seatsByEvent);
+			SeatsByEventDao::getInstance()->add($seatsByEvent);*/
 			require "Views/admin.php";
 		}
 	}
