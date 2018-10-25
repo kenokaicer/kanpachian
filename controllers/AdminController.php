4078d2079bee8dee2b24a0e63ae05815be634449
Mauro <maurogch@gmail.com>
2018-10-25 11:36:43 -0300

    lines for testing SeatsByEventDao

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,11 +1,20 @@
 <?php
 	namespace Controllers;
-	use Dao\BD\TheaterDao as TheaterDao;
+	use Dao\BD\SeatsByEventDao as SeatsByEventDao;
+	use Models\SeatsByEvent as SeatsByEvent;
+	use Models\SeatType as SeatType;
 	
 	class AdminController{
 		
 		function index()
 		{	
+			$seatType = new seatType();
+			$seatType->setIdSeatType(1)->setName('seat')->setDescription('desc');
+
+			$seatsByEvent = new SeatsByEvent();
+			$seatsByEvent->setQuantity(1)->setPrice(10)->setRemnants(100)->setSeatType($seatType);
+
+			SeatsByEventDao::getInstance()->add($seatsByEvent);
 			require "Views/admin.php";
 		}
 	}
