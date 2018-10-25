79ff10bd676fc9f6f0a4ec1efe3786a6b26fbdd0
Mauro <maurogch@gmail.com>
2018-10-25 08:12:25 -0300

    put a shortcut to admin

--- views/navbar.php
+++ views/navbar.php
@@ -1,5 +1,5 @@
 <ul class="vertical medium-horizontal menu">
-  <li><a href="#0"><i class="fi-list"></i> <span>One</span></a></li>
+  <li><a href="<?=FRONT_ROOT?>Admin"><i class="fi-list"></i> <span>Admin</span></a></li>
   <li><a href="#0"><i class="fi-list"></i> <span>Two</span></a></li>
   <li><a href="#0"><i class="fi-list"></i> <span>Three</span></a></li>
   <li><a href="#0"><i class="fi-list"></i> <span>Four</span></a></li>
