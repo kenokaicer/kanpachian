6fbdb334541d4cfe690a22755ddd8687f08026c4
Mauro <maurogch@gmail.com>
2018-10-25 09:15:15 -0300

    rename

---
+++ dao/BD/_Queries.sql
@@ -0,0 +1,20 @@
+USE neonlab1_gotoevent;
+
+/*insert into SeatTypes (name, description)
+values ('Palco', 'Palco');*/
+
+
+select *
+from Theaters;
+
+select * 
+from SeatTypes_x_Theater;
+
+
+
+
+
+
+
+
+
