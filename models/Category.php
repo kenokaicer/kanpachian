2e9b46a997870720a61926158daf78f566aaa51f
Mauro <maurogch@gmail.com>
2018-10-25 05:39:59 -0300

    enum to class

--- models/Category.php
+++ models/Category.php
@@ -2,11 +2,29 @@
 
 namespace Models;
 
-use SplEnum as SplEnum;
-
-class Category extends SplEnum
+class Category
 {
-    const recital = "Recital";
-    const boxeo = "Boxeo";
-    const obrateatral = "Obra Teatral";
+    private $category;
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getAll()
+    {
+        return get_object_vars($this);
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
 }
