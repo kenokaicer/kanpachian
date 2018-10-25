be0344c70ecbbc71ff928132d9178b84ca8e419c
Mauro <maurogch@gmail.com>
2018-10-25 07:08:57 -0300

    fix method naming

--- dao/Interfaces/ITheaterDao.php
+++ dao/Interfaces/ITheaterDao.php
@@ -6,8 +6,8 @@ use Models\Theater as Theater;
 interface ITheaterDao
 {
     public function Add(Theater $theater);
-    public function Get($var);
+    public function getByID($id);
     public function getAll();
     public function Update(Theater $oldTheater, Theater $newTheater);
-    public function Delete($id);
+    public function Delete(Theater $theater);
 }
