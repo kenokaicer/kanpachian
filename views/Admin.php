139b20c1ec3865a923e8c0214ecb1c4ec68370b9
Mauro <maurogch@gmail.com>
2018-10-25 11:36:55 -0300

    new controller buttons

--- views/Admin.php
+++ views/Admin.php
@@ -1,11 +1,13 @@
 <?php
 ?>
 <div class="wrapper"> 
-<h3>Temprary Admin view</h3>
+<h3>Administrador</h3>
 <form method="POST">
     <section>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>ArtistManagement/index">Gestión Artistas</button></div>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>TheaterManagement/index">Gestión Teatros</button></div>
+        <div><button type="submit" formaction="<?=FRONT_ROOT?>CategoryManagement/index">Gestión Categorías</button></div>
+        <div><button type="submit" formaction="<?=FRONT_ROOT?>SeatTypeManagement/index">Gestión Tipos de Asiento</button></div>
     </section>
     <section>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>Main/index">Volver (temp)</button></div>
