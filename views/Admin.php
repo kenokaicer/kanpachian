8d12348703ba2a770044c6ac6d91f246987aa93f
Mauro <maurogch@gmail.com>
2018-10-26 00:29:47 -0300

    set button width 100%

--- views/Admin.php
+++ views/Admin.php
@@ -4,15 +4,16 @@
 <h3>Administrador</h3>
 <form method="POST">
     <section>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>ArtistManagement/index">Gestión Artistas</button></div>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>TheaterManagement/index">Gestión Teatros</button></div>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>CategoryManagement/index">Gestión Categorías</button></div>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>SeatTypeManagement/index">Gestión Tipos de Asiento</button></div>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>EventManagement/index">Gestión Eventos</button></div>
-        <div><button type="submit" formaction="<?=FRONT_ROOT?>EventByDateManagement/index">Gestión Calendarios</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>ArtistManagement/index">Gestión Artistas</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>TheaterManagement/index">Gestión Teatros</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>CategoryManagement/index">Gestión Categorías</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>SeatTypeManagement/index">Gestión Tipos de Asiento</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>EventManagement/index">Gestión Eventos</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>EventByDateManagement/index">Gestión Calendarios</button></div>
     </section>
     <section>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>Main/index">Volver (temp)</button></div>
     </section>
+    Mirá papá esos botones
 </form>
 </div>
