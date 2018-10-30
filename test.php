cdb1601919cd5cfe528d69c0afb293e04cd90423
Mauro <maurogch@gmail.com>
2018-10-30 01:34:57 -0300

    test

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
-require "Config/Config.php";
+require_once "config/Config.php";
+require_once "config/Autoload.php";
 
-echo CSS_PATH;
+use Config\Autoload as Autoload;
+use Dao\BD\EventByDateDao as EventByDateDao;
+use Exception as Exception;
+
+Autoload::start();
+
+
+try{
+    $var = EventByDateDao::getInstance()->getByID(1);
+}catch(Exception $ex){
+    echo $ex->getMessage();
+}
+
+
+var_dump($var);
