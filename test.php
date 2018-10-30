da20c4a426e30a4ac6e2573d250250c83c573279
Mauro <maurogch@gmail.com>
2018-10-30 17:18:14 -0300

    testing done

--- test.php
+++ test.php
@@ -4,22 +4,31 @@ require_once "config/Autoload.php";
 
 use Config\Autoload as Autoload;
 use Dao\BD\EventByDateDao as EventByDateDao;
+use Dao\BD\SeatsByEventDao as SeatsByEventDao;
 use Exception as Exception;
 use Models\Role as Role;
 
 Autoload::start();
 
-/*
 try{
-    $var = EventByDateDao::getInstance()->getByID(1);
+    $var = SeatsByEventDao::getInstance()->getByEventByDateID(1);
 }catch(Exception $ex){
     echo $ex->getMessage();
 }
 
+var_dump($var);
+
+/*
+try{
+    $var = EventByDateDao::getInstance()->getAll();
+}catch(Exception $ex){
+    echo $ex->getMessage();
+}
 
 var_dump($var);
 */
 
+/*
 $var = Role::getConstants();
 
 var_dump($var);
@@ -27,3 +36,4 @@ var_dump($var);
 foreach ($var as $key => $value) {
     echo key($value);
 }
+*/
