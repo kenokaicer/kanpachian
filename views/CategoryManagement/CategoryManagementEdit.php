7e70e2403b66cf9c3c3b24b6114a68d3822bc63f
Mauro <maurogch@gmail.com>
2018-10-30 02:13:15 -0300

    Restored from oblivion

---
+++ views/CategoryManagement/CategoryManagementEdit.php
@@ -0,0 +1,20 @@
+<div class="wrapper">
+    <section>
+        <form action="<?=FRONT_ROOT?>CategoryManagement/editCategory" method="post">
+        <input type="hidden" name="idOldCategory" value="<?=$oldCategory->getIdCategory()?>">
+            <table>
+                <tr>
+                    <td>Categoría: <input type="text" name="name" value="<?=$oldCategory->getCategory()?>"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div>
+                            <button type="submit">Modificar</button>
+                            <input type="submit" value="Cancelar" formaction="<?=FRONT_ROOT?>CategoryManagement/index" formnovalidate>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </section> 
+</div>
---
+++ views/CategoryManagement/CategoryManagementList.php
@@ -0,0 +1,50 @@
+<div class="wrapper">
+    <section>
+        <table>
+            <thead>
+                <th>Categoría</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </thead>
+            <tbody>
+            <?php
+            if (!empty($categoryList)) {
+                foreach ($categoryList as $category) {  
+                    $categoryValuesArray = $category->getAll();
+            ?>
+                <tr>
+                    <form method="post">
+                        <?php 
+                            foreach ($categoryValuesArray as $atribute => $value) { //print all atributes from object in a td each
+                                if($atribute=="idCategory"){
+                        ?>
+                                    <input type="hidden" name="idCategory" value="<?=$value?>">
+                        <?php 
+                                }else{
+                                    echo "<td>";
+                                    echo $value;
+                                    echo "</td>";
+                                }
+                            }
+                        ?>
+                        <td>
+                            <input type="submit" value="Editar" formaction="<?=FRONT_ROOT?>CategoryManagement/viewEditCategory"> 
+                        </td>
+                        <td>
+                            <input type="submit" value="Eliminar" formaction="<?=FRONT_ROOT?>CategoryManagement/deleteCategory">
+                        </td>
+                    </form>
+                </tr>
+            <?php
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+    </section>
+    <section>
+        <form method="post">
+            <button type="submit" formaction="<?=FRONT_ROOT?>CategoryManagement/index">Volver</button>
+        </form>
+    </section>
+</div>
