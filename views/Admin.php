d1492eca3e59ef3033b57266c7a667984e621967
Mauro <maurogch@gmail.com>
2018-10-30 16:37:02 -0300

    add seatsbyevent

--- views/Admin.php
+++ views/Admin.php
@@ -10,11 +10,11 @@
         <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>SeatTypeManagement/index">Gestión Tipos de Asiento</button></div>
         <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>EventManagement/index">Gestión Eventos</button></div>
         <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>EventByDateManagement/index">Gestión Calendarios</button></div>
+        <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>SeatsByEventManagement/index">Gestión Asiento por Evento</button></div>
         <div><button style="width: 100%" type="submit" formaction="<?=FRONT_ROOT?>UserManagement/index">Gestión Usuarios</button></div>
     </section>
     <section>
         <div><button type="submit" formaction="<?=FRONT_ROOT?>Main/index">Volver (temp)</button></div>
     </section>
-    Mirá papá esos botones
 </form>
 </div>
