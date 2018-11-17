a7392e074d02b98c61627044e4f65f4306059361
Mauro <maurogch@gmail.com>
2018-11-17 01:01:42 -0300

    fix ticket errors

--- models/Ticket.php
+++ models/Ticket.php
@@ -9,35 +9,14 @@ class Ticket extends Attributes
     private $theater; // class
     private $date;
 
-    public __construct($_idTicket,$_ticketCode,$_qrCode,$_theater,$_date)
-    {
-        $this->idTicket = $_idTicket;
-        $this->ticketCode = $_ticketCode;
-        $this->qrCode = $_qrCode;
-        $this->theater = $_theater;
-        $this->date = $_date;
-    }   
-
-    public function getIdTicket()
-    {
-        return $this->idTicket;
-    }
+	public function getIdTicket()
+	{
+		return $this->idTicket;
+	}
 
     public function setIdTicket($idTicket)
     {
         $this->idTicket = $idTicket;
-
-        return $this;
-    }
-
-    public function getQrCode()
-    {
-        return $this->qrCode;
-    }
-
-    public function setQrCode($qrCode)
-    {
-        $this->qrCode = $qrCode;
 
         return $this;
     }
@@ -50,6 +29,42 @@ class Ticket extends Attributes
     public function setTicketCode($ticketCode)
     {
         $this->ticketCode = $ticketCode;
+
+        return $this;
+    }
+
+	public function getQrCode()
+	{
+		return $this->qrCode;
+	}
+
+    public function setQrCode($qrCode)
+    {
+        $this->qrCode = $qrCode;
+
+        return $this;
+    }
+
+	public function getTheater()
+	{
+		return $this->theater;
+	}
+
+    public function setTheater($theater)
+    {
+        $this->theater = $theater;
+
+        return $this;
+    }
+
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+    public function setDate($date)
+    {
+        $this->date = $date;
 
         return $this;
     }
