6e5d0f17af8919f56f9bad287c3bd6027742bcf5
maurogch <maurogch@gmail.com>
2018-11-21 21:14:08 -0300

    check total price per category and event, in progress

--- views/SalesByDateOrCategory.php
+++ views/SalesByDateOrCategory.php
@@ -1,11 +1,11 @@
 <body style="background-image: url('<?=IMG_PATH?>adminBackground.jpg');">
     
 <h2 style="color:white">Ventas</h2>
-<div class="wrapper" style="border-style:none;min-height:58vh;width:800px">
+<div class="wrapper" style="border-style:none;min-height:58vh;width:900px">
     <section class="app-feature-section">
         <div class="row align-middle">
             <div class="small-12 medium-12 columns" >
-                <h3 class="app-feature-section-main-header">Seleccione Categoría o Fecha</h3>    
+                <h3 class="app-feature-section-main-header">Seleccione por Fecha</h3>    
                 <!--<h4 class="app-feature-section-sub-header" style="display:inline-block">TEXTO</h4>-->
                 <div>
                     <form action="" onsubmit="return getTotal()">
@@ -17,20 +17,29 @@
                 <div>
                     <input style="margin:0 auto;width:200px" type="text" name="totalSale" id="totalSale" readonly>
                 </div>
+                </section>
+                <section class="app-feature-section">
+                <h3 class="app-feature-section-main-header">Seleccione Fecha de la Categoría</h3>    
                 <div>
+                <input type="date" name="date2" id="date2">
                     <table id="main-table">
                         <thead>
                             <th>Categoría</th>
                             <th>Total</th>
+                            <th>Ver</th>
+                            <th>Total por día</th>
                         </thead>
                         <tbody id="table-body">
                             <?php
+                            $i = 0;
                             if(isset($totalPriceByCategoryArray)){
                                 foreach ($totalPriceByCategoryArray as $cat => $price) {
                             ?>
                                 <tr>
-                                    <td><?=$cat?></td>
-                                    <td>$<?=$price?></td>
+                                    <td style="width:40%" id="catName<?=$i?>"><?=$cat?></td>
+                                    <td style="width:20%">$<?=$price?></td>
+                                    <td style="width:20%"><div style="margin: 0 auto"><input type="button" onclick="getTotalCat(<?php echo 'catName'.$i.',totalPriceCat'.$i?>)" value="ver"></div></td>
+                                    <td style="width:20%" id="catTotal<?=$i?>"><input style="margin:0" type="number" id="totalPriceCat" readonly></td>
                                 </tr>
                             <?php
                                 }
@@ -59,6 +68,20 @@ function getTotal()
     }); 
 
     return false
+}
+
+function getTotalCat(idCatName,idTotalPrice)
+{
+    var catName = document.getElementById ( "idCatName" ).innerText;
+    var date2 = document.getElementById("date2").value;
+
+    console.log(date2);
+    /*$("#loading").show();
+    $.when(ajaxQuery('getTotalByDate',date1)).done(function(ajaxResponse){ //waits for ajax call to be done
+        ajaxResponse = "$" + ajaxResponse;
+        document.getElementById("totalSale").value = ajaxResponse;
+        $("#loading").hide();
+    }); */
 }
 
 function ajaxQuery(func,value)
